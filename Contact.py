a698a19dbd231ec83df950421b5d9e31203ae49a
sanjay <sanjayhcu01@gmail.com>
2016-07-14 12:57:39 +0530

    Class with ContactManager Class

--- Contact.py
+++ Contact.py
@@ -27,7 +27,7 @@ class ContactOperation():
 Airtel mobile numbers starts with 9900, 9800, 9811
 BSNL mobile numbers starts with 9440, 9822
 Idea mobile numbers starts with 9848, 9912
-Reliance mobile numbers starts with 9300,
+Reliance mobile numbers starts with 9300,9852
 
     '''
     global provider
